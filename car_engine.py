261d2ad8c0e957e8075ca1b376ebdbe06f105740
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-09-02 23:05:45 +0500

    car_engine program

---
+++ car_engine.py
@@ -0,0 +1,13 @@
+user_input = input().casefold()
+
+while(user_input != 'quit'):
+    if user_input == 'start':
+        print('Car started ...')
+    elif user_input == 'stop':
+        print('Car stopped ...')
+    else:
+        print("I don't understand that")
+
+    user_input = input().casefold()
+
+print('You left the car.')
