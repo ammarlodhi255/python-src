d658a35217428dadac2703ac789e2b402b9144b0
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-09-02 19:42:46 +0500

    OutputDigitsOfNum

--- OutputDigitsOfNum.py
+++ OutputDigitsOfNum.py
@@ -10,7 +10,7 @@ list_of_digits.reverse()
 digits_str = " ".join(list_of_digits)
 print(digits_str)
 
-#Second method:
+# Second method:
 list_of_digits.reverse()
-for i in range(len(list_of_digits)): print(list_of_digits.pop(), end = " ")
-
+for i in range(len(list_of_digits)):
+    print(list_of_digits.pop(), end=" ")
