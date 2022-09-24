a288de9785557ab795439818a9276a5d5c5fc33b
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-09-24 23:28:26 +0500

    enumerate in loops

--- tips_python/enumerate.py
+++ tips_python/enumerate.py
@@ -2,3 +2,12 @@ areas = [56.0, 20.5, 78.2, 45.8, 82.9]
 
 for index, area in enumerate(areas):
     print(f'Room {index + 1}: {area} sq. ft.')
+
+
+# Example where enumerate is useful:
+
+hotels = ['Crowne Plaza', 'Emerald Bay Inn', 'Hotel Bliss', 'University Inn', 'The New View']
+areas = [56.0, 20.5, 78.2, 45.8, 82.9]
+
+for index, hotel in enumerate(hotels):
+    print(f'Hotel: {hotel}, Room area: {areas[index]}')
