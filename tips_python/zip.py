84a0105def2b3b67dcb80a489931d18186849f45
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-09-24 23:54:48 +0500

    zip to unpack values from various lists

--- tips_python/zip.py
+++ tips_python/zip.py
@@ -1,6 +1,7 @@
 hotels = ['Crowne Plaza', 'Emerald Bay Inn',
           'Hotel Bliss', 'University Inn', 'The New View']
 areas = [56.0, 20.5, 78.2, 45.8, 82.9]
+rooms = [8, 9, 11, 25, 112]
 
-for hotel, area in zip(hotels, areas):
-    print(f'{hotel} with room area: {area}')
+for hotel, area, room in zip(hotels, areas, rooms):
+    print(f'{hotel} with Room No. {room} has area {area} sqft.')
