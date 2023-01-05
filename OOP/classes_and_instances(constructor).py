003a5749e253c7520cf77b4da22c432a79ed475d
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-01-05 15:30:00 +0500

    class with a body, logical grouping of data and methods

--- OOP/classes_and_instances(constructor).py
+++ OOP/classes_and_instances(constructor).py
@@ -5,4 +5,17 @@ class Car:
         self.speed = speed
 
 
-car_1 = ()
+    def show_details(self):
+        print(f'Color: {self.color}\nSpeed: {self.speed}\nModel: {self.model}\n')
+
+'''
+Note we do not have to pass in the argument for 'self' parameter.
+The object that we instantiate is automatically passed in for the self parameter.
+'''
+car_1 = Car('red', 'E12', '12mph')
+car_2 = Car('green', 'R14', '100mph')
+
+car_1.show_details()
+car_2.show_details()
+
+
