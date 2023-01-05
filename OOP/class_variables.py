83cded7223f8e2f5ab0892db7d48b97f71b87bb5
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-01-05 16:29:48 +0500

    class variables

---
+++ OOP/class_variables.py
@@ -0,0 +1,20 @@
+class Car:
+
+    num_of_chairs = 4  # Class Variable
+
+    def __init__(self, color, model, speed):
+        self.color = color
+        self.model = model
+        self.speed = speed
+
+    def show_details(self):
+        print(
+            f'Color: {self.color}\nSpeed: {self.speed}\nModel: {self.model}\n')
+
+    def show_num_of_chairs(self):
+        print(Car.num_of_chairs)  # Access class variable
+
+
+car = Car('blue', 'OP90', '190mph')
+# Let us see the instance variable of this object:
+print(car.__dict__)
