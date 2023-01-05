f327f6f33f04df4064d4c780b2f95cc1c895f12a
ammarahmed69 <121630002+ammarahmed69@users.noreply.github.com>
2023-01-05 14:53:00 +0500

    other attributes

--- OOP/classes_and_instances.py
+++ OOP/classes_and_instances.py
@@ -1,10 +1,19 @@
 class Car:
     pass
 
+
 car_1 = Car()
 car_2 = Car()
 
 car_1.color = 'red'
 car_2.color = 'green'
 
-print(car_1.color, car_2.color)
+car_1.model = 'E12'
+car_1.speed = '12mph'
+
+car_2.model = 'R14'
+car_2.speed = '100mph'
+
+
+print(f'Car 1: {car_1.color}, {car_1.model}, {car_1.speed}')
+print(f'Car 2: {car_2.color}, {car_2.model}, {car_2.speed}')
