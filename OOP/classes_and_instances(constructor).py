edf70d5ffbb7c2994ce6781f6b2fe187e27e3aba
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-01-05 16:06:35 +0500

    alternative way to call a method

--- OOP/classes_and_instances(constructor).py
+++ OOP/classes_and_instances(constructor).py
@@ -11,7 +11,7 @@ class Car:
 
 
 '''
-Note we do not have to pass in the argument for 'self' parameter.
+Note we do not have to pass in the argument for 'self' parameter when any method inside a class.
 The object that we instantiate is automatically passed in for the self parameter.
 '''
 car_1 = Car('red', 'E12', '12mph')
@@ -19,6 +19,9 @@ car_2 = Car('green', 'R14', '100mph')
 
 car_1.show_details()
 car_2.show_details()
+
+# An alternative way to call a function:
+Car.show_details(car_1)  # Call show_details for the instance 'car_1'
 
 # If you leave the parenthesis off, when refering to the method, you will get the following output:
 print(car_1.show_details)
