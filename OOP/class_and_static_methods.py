1993f38723c59e1e70d69cb461cb7da17c4ab201
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-01-06 15:10:03 +0500

    class methods

--- OOP/class_and_static_methods.py
+++ OOP/class_and_static_methods.py
@@ -14,4 +14,20 @@ class Car:
     def show_num_of_chairs(self):
         print(Car.num_of_chairs)  # Access class variable
 
+    @classmethod
+    def set_num_of_chairs(cls, num_of_chairs):
+        cls.num_of_chairs = num_of_chairs
 
+
+car_1 = Car('red', 'UN', '100')
+car_2 = Car('red', 'UN1', '100')
+
+print(car_1.num_of_chairs)
+print(car_2.num_of_chairs)
+print(Car.num_of_chairs)
+
+Car.set_num_of_chairs(5)
+
+print(car_1.num_of_chairs)
+print(car_2.num_of_chairs)
+print(Car.num_of_chairs)
